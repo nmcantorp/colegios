f0c9bab6a8dcc214238324dae0a045c2a7de258a
Stefano105 <aiorosns@gmail.com>
2016-01-12 14:09:55 -0500

    clase cargos
    Para uso de transacciones con la clase cargos y creación de formulario

---
+++ clases/cargos.php
@@ -0,0 +1,58 @@
+<?php 
+session_start()                                                                                                                                                         ;
+
+/**
+* Clase para la funcionalidad del logueo de los usuarios 
+*/
+include_once('conexion.php');
+
+class ClassCargos extends ClassConexion
+{
+    /**
+    * Valida la información del logueo 
+    * @param varchar $usuario
+    * @param varchar $pass
+    * 
+    * return boolean   
+    */
+    function get_Cargos()
+    {
+
+        $db = new ClassConexion();
+        $ingreso = false;
+
+        $query="SELECT
+                cargos.id_cargo,
+                cargos.descripcion_cargo,
+                cargos.observaciones,
+                cargos.activo,
+                cargos.fecha_creacion,
+                cargos.usuario_creador,
+                cargos.fecha_modificacion,
+                cargos.usuario_modificador
+                FROM cargos";
+
+        $consulta = $db->consulta($query);		
+          
+        return $consulta;  
+    }
+
+    function ing_cargos()
+    {
+        $db = new ClassConexion();
+        $ingreso = false;
+
+        $query = "call insertar_cargos('Profesor','Profesor de bachillerato','S','ADMIN',Now())";
+
+        $consulta = $db->consulta($query);
+
+        return $consulta;
+
+
+
+    }
+}
+
+
+
+?>
